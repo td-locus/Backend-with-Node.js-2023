fcf3893be6a0d027de0eac4b796d465ad71b2a3d
kunalkeshan <kunalkeshan12@gmail.com>
2023-07-06 09:38:54 +0530

    chore: basic setup for mysql

---
+++ Session 5 - Templating Using EJS/utils/database.js
@@ -0,0 +1,12 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'test'
+});
+
+module.exports = connection;
+
